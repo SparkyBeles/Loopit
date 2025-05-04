d4abb113cc6b9d0f3f2580a64edb30f9e5521b9c
beles <181732444+SparkyBeles@users.noreply.github.com>
2025-05-04 15:31:32 +0200

    Initial Commit

---
+++ Loopit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loopit
+//
+//  Created by Beles on 2025-05-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
