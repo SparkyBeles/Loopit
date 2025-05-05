2b17e6668e6c26dcfb5527b24eb51e0cdddfdd2e
beles <181732444+SparkyBeles@users.noreply.github.com>
2025-05-05 23:33:23 +0200

    a simple login vc.

--- Loopit/CreateAccountVC.swift
+++ Loopit/CreateAccountVC.swift
@@ -4,26 +4,51 @@
 //
 //  Created by Beles on 2025-05-04.
 //
-
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class CreateAccountVC: UIViewController {
-
+    
+    @IBOutlet weak var name_textField: UITextField!
+    @IBOutlet weak var email_textField: UITextField!
+    @IBOutlet weak var password_textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func create_Account_Button(_ sender: Any) {
+        guard let name = name_textField.text, !name.isEmpty,
+              let email = email_textField.text, !email.isEmpty,
+              let password = password_textField.text, !password.isEmpty else {
+            print("Fyll i alla fält.")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Fel vid kontoskapande: \(error.localizedDescription)")
+            } else if let user = authResult?.user {
+                print("Konto skapat för: \(user.email ?? "okänd")")
+                
+                let db = Firestore.firestore()
+                db.collection("users").document(user.uid).setData([
+                    "name": name,
+                    "email": email,
+                    "createdAt": Timestamp()
+                ]) { error in
+                    if let error = error {
+                        print("Kunde inte spara namn: \(error.localizedDescription)")
+                    } else {
+                        print("Namn sparat i Firestore.")
+                        self.dismiss(animated: true)
+                    }
+                }
+            }
+        }
+        
     }
-    */
-
+    
+    
 }
--- Loopit/ViewController.swift
+++ Loopit/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email_text_field: UITextField!
+    @IBOutlet weak var password_text_field: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func login_button(_ sender: Any) {
+        guard let email = email_text_field.text, !email.isEmpty,
+              let password = password_text_field.text, !password.isEmpty else {
+            print("E-post och lösenord måste fyllas i.")
+            return
+        }
 
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Inloggning misslyckades: \(error.localizedDescription)")
+            } else {
+                print("Inloggad som: \(authResult?.user.email ?? "okänd")")
+                self.navigateToMainScreen()
+            }
+        }
+    }
+
+    func navigateToMainScreen() {
+        performSegue(withIdentifier: "toHomeVC", sender: self)
+    }
 }
-
